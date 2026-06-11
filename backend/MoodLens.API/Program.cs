06197683ab1b26f2a1a100fd37b8267937e892aa
DESKTOP-RK9I1BU\ADMIN <attran123456@gmail.com>
2026-06-11 22:22:04 +0700

    Edit env backend 5

--- backend/MoodLens.API/Program.cs
+++ backend/MoodLens.API/Program.cs
@@ -120,11 +120,11 @@ builder.Services.AddDbContext<MoodLensDbContext>(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 app.UseCors("AllowFrontend");
 //JWT
 app.UseAuthentication();
